b08b1b38774c57713dc0f234abadb946c8b703cc
Brian Genisio <briangenisio@gmail.com>
2013-04-19 21:14:57 -0400

    Asking a question

--- scripts/app.js
+++ scripts/app.js
@@ -10,10 +10,29 @@ window.App.factory('User', function() {
 	};
 });
 
+window.App.factory('Questions', function() {
+	return [];
+});
+
 window.App.controller('LoginCtrl', function($scope, User) {
 	$scope.user = User;
 });
 
-window.App.controller('AskAQuestionCtrl', function($scope, User) {
+window.App.controller('AskAQuestionCtrl', function($scope, User, Questions) {
 	$scope.user = User;
+	$scope.questions = Questions;
+
+	$scope.add = function(questionText) {
+		var newQuestion = {
+			text: questionText,
+			userName: User.name
+		};
+
+		Questions.push(newQuestion);
+		$scope.newQuestionText = '';
+	}
+});
+
+window.App.controller('QuestionListCtrl', function($scope, Questions) {
+	$scope.questions = Questions;
 });
